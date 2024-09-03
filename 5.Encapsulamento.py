63eaad2eb5c625ed92b0fc1fd250648464918fb1
S4L01 <159718065+S4L01@users.noreply.github.com>
2024-09-03 14:32:37 +0000

    AttDia03

---
+++ 5.Encapsulamento.py
@@ -0,0 +1,5 @@
+import os 
+
+class conta:
+    def __init__ 
+
---
+++ 6.Atividade.py
@@ -0,0 +1,29 @@
+from abc import ABC,abstractmethod
+import os 
+
+os.system("cls||clear")
+
+class Endereco:
+    def __init__(self, logradouro:str,numero:str,cidade:str):
+        self.logradouro = logradouro
+        self.numero = numero
+        self.cidade = cidade 
+
+class Funcionario(ABC):
+    def __init__ (self,nome:str,email:str,salario:float, endereco:Endereco):
+        self.nome = nome  
+        self.email = email  
+        self.salario = salario  
+        self.endereco = endereco  
+    @abstractmethod
+    def salario_final(self)--float 
+        pass
+    def __str__ (self):
+        return (f"\nNome:{self.nome}"
+                f"\nEmail:{self.email}"
+                f"\nSalario:{self.salario}"
+                f"\nEndereco:{self.endereco}"
+        )    
+
+class Motoboy(Funcionario):
+    def __init__ (self,nome):
